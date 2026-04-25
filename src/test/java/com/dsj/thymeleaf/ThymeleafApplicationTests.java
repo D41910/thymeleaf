07e3052e798642e897df32c17b31096ef365a56e
dongsj <dongsijun@dongsijundeMacBook-Air.local>
2026-04-25 09:16:11 +0800

    feat:初始化

---
+++ src/test/java/com/dsj/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsj.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

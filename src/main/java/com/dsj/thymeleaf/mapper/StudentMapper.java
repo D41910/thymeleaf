f1f1941a1517c7534c54d2f7f9563293bbce72f6
dongsj <dongsijun@dongsijundeMacBook-Air.local>
2026-04-25 09:28:43 +0800

    fix:学号问题修复

--- src/main/java/com/dsj/thymeleaf/mapper/StudentMapper.java
+++ src/main/java/com/dsj/thymeleaf/mapper/StudentMapper.java
@@ -2,11 +2,20 @@ package com.dsj.thymeleaf.mapper;
 
 import com.dsj.thymeleaf.entity.Student;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 @Mapper
 public interface StudentMapper {
     @Select("select * from student")
+    @Results({
+        @Result(column = "student_id", property = "studentId"),
+        @Result(column = "name", property = "name"),
+        @Result(column = "age", property = "age"),
+        @Result(column = "sex", property = "sex"),
+        @Result(column = "birthday", property = "birthday")
+    })
     List<Student> findAll();
 }
